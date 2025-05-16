124a5b620c57abd287b0f2e0842663fcae4f2472
E-RKP <edobackup111@gmail.com>
2025-05-16 17:37:49 +0200

    added counter

--- data.js
+++ data.js
@@ -17,3 +17,83 @@ function messaggio(){
 button.addEventListener('click', ()=>{
   messaggio();
 })
+
+const secondButton = document.getElementById('sbottoncino')
+const lista = document.getElementById('lista')
+const nomi = ['Mario', 'Luca', 'Gianni', 'Beppe', 'Maria']
+let listoide = []
+function stampafive(){
+
+  
+  for (i=0; i<nomi.length;i++){
+    console.log(nomi[i])
+    listoide.push(nomi[i])
+  }
+  return listoide;
+}
+
+secondButton.addEventListener('click', ()=>{
+  stampafive();
+  lista.textContent = listoide;
+})
+
+const stringaNumeri = document.getElementById('numeri')
+const bottonen = document.getElementById ('sbottoncine')
+const lis = document.getElementById('list')
+
+
+bottonen.onclick = function a (){
+
+let newArr = stringaNumeri.value.split(' ');
+for (i=0; i<newArr.length;i++){
+console.log(newArr[i])
+}
+console.log(newArr)
+
+}
+
+const numerino = document.getElementById('figuraccia')
+const numerone = document.getElementById('numerone')
+const diminuire = document.getElementById('diminuisce')
+const aggiungere = document.getElementById('aggiungere')
+let counterBig = 0
+let counterSmall = 0
+
+
+function counterp (){
+
+  counterBig++;
+  counterSmall++;
+  return {
+    counterBig,
+    counterSmall
+  };
+}
+
+function counterm () {
+  counterSmall++;
+  counterBig--;
+
+  if (counterBig < 0){
+    counterBig = 0
+  }
+
+  return {
+    counterBig,
+    counterSmall
+  };
+}
+
+diminuire.addEventListener('click', ()=>{
+  counterm();
+  numerino.innerHTML = counterSmall
+  numerone.innerHTML = counterBig
+})
+
+aggiungere.addEventListener('click', ()=>{
+  counterp();
+  numerino.innerHTML = counterSmall
+  numerone.innerHTML = counterBig
+})
+
+
